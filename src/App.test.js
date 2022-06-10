2a4589b8a066e00df6d84e253de47a0acfa577a9
frankieavina <frankieavina@gmail.com>
2022-06-10 14:23:33 -0700

    started testing

--- src/App.test.js
+++ src/App.test.js
@@ -7,7 +7,7 @@ import App from './App';
 import Header from './components/Header/Header';
 
 test(
-  'User should input a city and get autocomplete city',
+  'User should input a city and get autocomplete city name',
   () => {
     render(<Header />);
     const input = screen.getByRole('textbox');
@@ -19,3 +19,17 @@ test(
     expect(input).toHaveValue('bar');
   }
 );
+
+test('the data is peanut butter', async () => {
+  const data = await fetchData();
+  expect(data).toBe('peanut butter');
+});
+
+test('the fetch fails with an error', async () => {
+  expect.assertions(1);
+  try {
+    await fetchData();
+  } catch (e) {
+    expect(e).toMatch('error');
+  }
+});
